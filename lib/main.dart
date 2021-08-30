498c298c81d508c421c45a338c2785ae2dde6ae6
Tarlison <tarlison_brito@hotmail.com>
2021-08-30 19:28:33 -0400

    modularization of the appWidget+Config firebase function in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,42 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:payflow_flutterapp/modules/home/home_page.dart';
-import 'package:payflow_flutterapp/modules/splash/splash_page.dart';
-import 'package:payflow_flutterapp/shared/themes/app_colors.dart';
-import 'package:payflow_flutterapp/modules/login/login_page.dart';
+import 'package:payflow_flutterapp/app_widget.dart';
 
 void main() {
-  runApp(AppWidget());
+  runApp(AppFirebase());
 }
 
-class AppWidget extends StatelessWidget {
+class AppFirebase extends StatefulWidget {
   // This widget is the root of your application.
   @override
+  _AppFirebaseState createState() => _AppFirebaseState();
+}
+
+class _AppFirebaseState extends State<AppFirebase> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pay Flow',
-      theme: ThemeData(
-        primaryColor: AppColors.primary,
-      ),
-      home: SplashPage(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Material(
+              child: Center(
+            child: Text("Não foi possível linicializar o Firebase",
+                textDirection: TextDirection.ltr),
+          ));
+        } else if (snapshot.connectionState == ConnectionState.done) {
+          return AppWidget();
+        } else {
+          return Material(
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
     );
   }
 }
