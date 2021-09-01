958b3ec094c834c1bb5d96eb10705b93b917993f
Tarlison <tarlison_brito@hotmail.com>
2021-09-01 16:40:54 -0400

    finalizando

--- lib/shared/widgets/boleto_tile/boleto_tile_widget.dart
+++ lib/shared/widgets/boleto_tile/boleto_tile_widget.dart
@@ -1,3 +1,4 @@
+import 'package:animated_card/animated_card.dart';
 import 'package:flutter/material.dart';
 import 'package:payflow_flutterapp/shared/models/boleto_model.dart';
 import 'package:payflow_flutterapp/shared/themes/app_text_styles.dart';
@@ -8,22 +9,25 @@ class BoletoTileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      contentPadding: EdgeInsets.zero,
-      title: Text(
-        data.name!,
-        style: TextStyles.titleListTile,
+    return AnimatedCard(
+      direction: AnimatedCardDirection.right,
+      child: ListTile(
+        contentPadding: EdgeInsets.zero,
+        title: Text(
+          data.name!,
+          style: TextStyles.titleListTile,
+        ),
+        subtitle: Text(
+          "Vence em ${data.dueDate}",
+          style: TextStyles.captionBody,
+        ),
+        trailing: Text.rich(
+            TextSpan(text: "R\$", style: TextStyles.trailingRegular, children: [
+          TextSpan(
+              text: "${data.value!.toStringAsFixed(2)}",
+              style: TextStyles.trailingBold)
+        ])),
       ),
-      subtitle: Text(
-        "Vence em ${data.dueDate}",
-        style: TextStyles.captionBody,
-      ),
-      trailing: Text.rich(
-          TextSpan(text: "R\$", style: TextStyles.trailingRegular, children: [
-        TextSpan(
-            text: "${data.value!.toStringAsFixed(2)}",
-            style: TextStyles.trailingBold)
-      ])),
     );
   }
 }
--- lib/shared/widgets/input_text/input_text_widget.dart
+++ lib/shared/widgets/input_text/input_text_widget.dart
@@ -1,3 +1,4 @@
+import 'package:animated_card/animated_card.dart';
 import 'package:flutter/material.dart';
 import 'package:payflow_flutterapp/shared/themes/app_colors.dart';
 import 'package:payflow_flutterapp/shared/themes/app_text_styles.dart';
@@ -22,45 +23,48 @@ class InputTextWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 16),
-      child: Column(
-        children: [
-          TextFormField(
-            controller: controller,
-            onChanged: onChanged,
-            initialValue: initialValue,
-            validator: validator,
-            style: TextStyles.input,
-            decoration: InputDecoration(
-                contentPadding: EdgeInsets.zero,
-                labelText: label,
-                labelStyle: TextStyles.input,
-                icon: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 18),
-                      child: Icon(
-                        icon,
-                        color: AppColors.primary,
+    return AnimatedCard(
+      direction: AnimatedCardDirection.left,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 16),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: controller,
+              onChanged: onChanged,
+              initialValue: initialValue,
+              validator: validator,
+              style: TextStyles.input,
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.zero,
+                  labelText: label,
+                  labelStyle: TextStyles.input,
+                  icon: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 18),
+                        child: Icon(
+                          icon,
+                          color: AppColors.primary,
+                        ),
                       ),
-                    ),
-                    Container(
-                      width: 1,
-                      height: 48,
-                      color: AppColors.stroke,
-                    )
-                  ],
-                ),
-                border: InputBorder.none),
-          ),
-          Divider(
-            height: 1,
-            thickness: 1,
-            color: AppColors.stroke,
-          )
-        ],
+                      Container(
+                        width: 1,
+                        height: 48,
+                        color: AppColors.stroke,
+                      )
+                    ],
+                  ),
+                  border: InputBorder.none),
+            ),
+            Divider(
+              height: 1,
+              thickness: 1,
+              color: AppColors.stroke,
+            )
+          ],
+        ),
       ),
     );
   }
